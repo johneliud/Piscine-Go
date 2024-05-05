6988a098a77613af0502e643abe774ec09929cc0
JohnEliud <johneliud4@gmail.com>
2024-05-05 16:13:42 +0300

    Piscine-Go

---
+++ printalphabet.go
@@ -0,0 +1,14 @@
+// Write a program that prints the Latin alphabet in lowercase on a single line.
+package main
+
+import "github.com/01-edu/z01"
+
+func printAlphabet() {
+	for i := 'a'; i <= 'z'; i++ {
+		z01.PrintRune(i)
+	}
+}
+
+func main() {
+	printAlphabet()
+}
